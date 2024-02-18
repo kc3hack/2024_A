bfabd203d2c0551656b1345dec690724c037cd3b
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-18 18:48:48 +0900

    Add Navbar

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "../components/Navbar"
 
 const Header = () => {
   return (
@@ -6,6 +7,7 @@ const Header = () => {
       <div className="title">
         <h3>Atmosfeel</h3>
       </div>
+      <Navbar/>
     </header>
   );
 };
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+    <p>search</p>
+    <p>details</p>
+  </nav>
+  )
+}
+
+export default Navbar
