23702aee37e84d5d808bdc7004b2cafbca7f1892
itoxu110 <yitenghuixi@gmail.com>
2024-02-19 23:15:19 +0900

    エラー解消

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,77 +1,17 @@
-//import { useState } from "react";
-//import reactLogo from "../public/react.svg";
-//import viteLogo from "../public/vite.svg";
 import "../styles/App.css";
-import 'leaflet/dist/leaflet.css';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {DraggableMarker} from './DraggableMarker';
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 86ac4e3bca4a9e0ee51a11bec96136ba44c88d86
-//import {DraggableMarker} from './DraggableMarker';
-import { Weather } from './weather';
-=======
+import "leaflet/dist/leaflet.css";
+//import { DraggableMarker } from "./DraggableMarker";
+import { Weather } from "./weather";
+
 //Mapコンポーネントをインポート
-import {Map} from './Map.tsx';
->>>>>>> parent of ec11cef (位置情報の取得とマップの表示を行う)
-<<<<<<< HEAD
->>>>>>> 86ac4e3 (Revert "位置情報の取得とマップの表示を行う")
-=======
->>>>>>> 86ac4e3bca4a9e0ee51a11bec96136ba44c88d86
+import { Map } from "./Map.tsx";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <DraggableMarker />
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 86ac4e3bca4a9e0ee51a11bec96136ba44c88d86
       <Weather />
-=======
-      <Map />
->>>>>>> parent of ec11cef (位置情報の取得とマップの表示を行う)
-<<<<<<< HEAD
->>>>>>> 86ac4e3 (Revert "位置情報の取得とマップの表示を行う")
-=======
->>>>>>> 86ac4e3bca4a9e0ee51a11bec96136ba44c88d86
     </div>
   );
 }
 
 export default App;
-
-/*
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
-*/
