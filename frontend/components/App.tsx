86ac4e3bca4a9e0ee51a11bec96136ba44c88d86
itoxu110 <yitenghuixi@gmail.com>
2024-02-19 13:25:42 +0900

    Revert "位置情報の取得とマップの表示を行う"
    This reverts commit ec11cef3631d7d3d6c4ecd5d64c650b882756d80.

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,14 +1,57 @@
+//import { useState } from "react";
+//import reactLogo from "../public/react.svg";
+//import viteLogo from "../public/vite.svg";
 import "../styles/App.css";
 import 'leaflet/dist/leaflet.css';
+<<<<<<< HEAD
 //import {DraggableMarker} from './DraggableMarker';
 import { Weather } from './weather';
+=======
+//Mapコンポーネントをインポート
+import {Map} from './Map.tsx';
+>>>>>>> parent of ec11cef (位置情報の取得とマップの表示を行う)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Weather />
+=======
+      <Map />
+>>>>>>> parent of ec11cef (位置情報の取得とマップの表示を行う)
     </div>
   );
 }
 
 export default App;
+
+/*
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+*/
--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -6,7 +6,7 @@ export const Map = () => {
   // 緯度軽度
   const position = [51.505, -0.09];
   // 初期マップズームレベル
-  const zoom = 18;
+  const zoom = 13;
   return (
     <MapContainer center={position} zoom={zoom}>
       <TileLayer
