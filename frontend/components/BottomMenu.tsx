885933eb75619771576c05fcd2335546fa0c15c5
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-19 20:36:26 +0900

    add bottom menu icon

--- frontend/components/BottomMenu.tsx
+++ frontend/components/BottomMenu.tsx
@@ -4,9 +4,27 @@ import "../styles/BottomMenu.css";
 const BottomMenu = () => {
   return (
     <div className="bottom-menu">
-      <div className="add-music-button">add</div>
-      <div className="home-button">home</div>
-      <div className="music-button">music</div>
+      <div className="bottom-menu-element">
+        <div className="add-music">
+          <img src="/math-plus.svg" alt="" className="add-music-button" />
+          <br />
+          <span>追加</span>
+        </div>
+      </div>
+      <div className="bottom-menu-element">
+        <div className="home">
+          <img src="/home-alt.svg" alt="" className="home-button" />
+          <br />
+          <span>ホーム</span>
+        </div>
+      </div>
+      <div className="bottom-menu-element">
+        <div className="music">
+          <img src="/music.svg" alt="" className="music-button" />
+          <br />
+          <span>ミュージック</span>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/components/TopMenu.tsx
+++ frontend/components/TopMenu.tsx
@@ -11,7 +11,11 @@ const TopMenu = () => {
         <form>
           <div className="search-bar">
             <img className="search-icon" src="search.svg" alt="" />
-            <input className="search-box" type="search" placeholder="Search" />
+            <input
+              className="search-box"
+              type="search"
+              placeholder="曲を検索"
+            />
           </div>
         </form>
         <div>
