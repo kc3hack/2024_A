71688ec22c2506ade546c73ce7eed0f54b671e0b
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-20 21:55:19 +0900

    Style Music Menu

--- frontend/components/MusicMenu.tsx
+++ frontend/components/MusicMenu.tsx
@@ -4,27 +4,21 @@ import "../styles/MusicMenu.css";
 const MusicMenu = () => {
   return (
     <div className="music-menu">
-      <div className="music-menu-element">
-        <div className="music-description">MusicTitle</div>
+      <div className="music-description">MusicTitle</div>
+      <div className="music-menu-buttons">
+        <img
+          src="/play-track-prev.svg"
+          alt=""
+          className="play-track-prev-button"
+        />
+        <img src="/play-button.svg" alt="" className="play-track-button" />
+        <img
+          src="/play-track-next.svg"
+          alt=""
+          className="play-track-next-button"
+        />
       </div>
-      <div className="music-menu-element">
-        <div className="music-menu-buttons">
-          <img
-            src="/play-track-prev.svg"
-            alt=""
-            className="play-track-prev-button"
-          />
-          <img src="/play-button.svg" alt="" className="play-track-button" />
-          <img
-            src="/play-track-next.svg"
-            alt=""
-            className="play-track-next-button"
-          />
-        </div>
-      </div>
-      <div className="music-menu-element">
-        <div className="music-volume">Volume</div>
-      </div>
+      <div className="music-volume-control">Volume Control</div>
     </div>
   );
 };
