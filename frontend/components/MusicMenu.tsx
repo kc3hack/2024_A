98ce98204a0acffaed617a071cc630593108bfd3
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-20 19:20:45 +0900

    Style Music Menu

--- frontend/components/MusicMenu.tsx
+++ frontend/components/MusicMenu.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "../styles/MusicMenu.css";
 
 const MusicMenu = () => {
-  return <div className="music-menu">MusicMenu</div>;
+  return (
+    <div className="music-menu">
+      <div className="music-menu-element">
+        MusicTitle <br />
+        Artist
+      </div>
+      <div className="music-menu-element">MusicButtons</div>
+      <div className="music-menu-element">VolumeControl</div>
+    </div>
+  );
 };
 
 export default MusicMenu;
