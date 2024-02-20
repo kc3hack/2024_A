d2f9d00916416977c99d67cba7a72ebb2c12a091
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-20 22:24:45 +0900

    Add Volume Control

--- frontend/components/MusicMenu.tsx
+++ frontend/components/MusicMenu.tsx
@@ -4,7 +4,10 @@ import "../styles/MusicMenu.css";
 const MusicMenu = () => {
   return (
     <div className="music-menu">
-      <div className="music-description">MusicTitle</div>
+      <div className="music-description">
+        MusicTitle <br />
+        Artist
+      </div>
       <div className="music-menu-buttons">
         <img
           src="/play-track-prev.svg"
@@ -18,7 +21,16 @@ const MusicMenu = () => {
           className="play-track-next-button"
         />
       </div>
-      <div className="music-volume-control">Volume Control</div>
+      <div className="music-volume-control">
+        <img src="/volume-up.svg" alt="" className="volume-icon" />
+        <input
+          type="range"
+          name="volume-bar"
+          min="0"
+          max="100"
+          className="volume-bar"
+        ></input>
+      </div>
     </div>
   );
 };
