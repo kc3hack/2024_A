4eee73489597c14e0b8147776581096819d6e916
itoxu110 <yitenghuixi@gmail.com>
2024-02-20 22:00:11 +0900

    いらないコメントの削除

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,10 +1,6 @@
 import "../styles/App.css";
 import "leaflet/dist/leaflet.css";
-//import { DraggableMarker } from "./DraggableMarker";
 import { Weather } from "./weather";
-
-//Mapコンポーネントをインポート
-import { Map } from "./Map.tsx";
 
 function App() {
   return (
