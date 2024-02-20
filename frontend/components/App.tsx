9ae69a0e875d984a76cbff3d0d57cc7a78cbbd73
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-20 23:57:28 +0900

    Add Routing

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,12 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
-import "../styles/App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="Home">
-      <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Test />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
