090a262c2a897ea97fcce35403d586f1142dc9fd
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-20 16:31:38 +0900

    lay out Map and Value

--- frontend/components/Main.tsx
+++ frontend/components/Main.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import Map from "../components/Map";
+import Value from "../components/Value";
 // import Top from "../pages/Top";
 
 const Main = () => {
   return (
     <main>
       <Map />
-      <br />1
-      <br />2
-      <br />3
-      <br />4
-      <br />5
-      <br />6
-      <br />7
-      <br />8
-      <br />9{/* <Top /> */}
+      <Value />
+      {/* <Top /> */}
     </main>
   );
 };
---
+++ frontend/components/Value.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Value.css";
+
+const Value = () => {
+  return <div className="value">Value</div>;
+};
+
+export default Value;
