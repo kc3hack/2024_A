e3243127b95ab541f8c3a30ada810b88338a664e
JohnDoe-Jack <makaroni.aori0421@gmail.com>
2024-02-21 23:29:34 +0900

    正しいページを表示

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../styles/index.css";
-import Location from "../examples/Location.tsx";
+import App from "../components/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Location />
+    <App />
   </React.StrictMode>,
 );
