cdbe78d783723badd8c98bdb5e4cdecc3d59ce92
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-02-22 16:31:05 +0900

    仮

--- frontend/components/test3.tsx
+++ frontend/components/test3.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { search } from "./test2";
 import { musicData } from "./App";
 import "../styles/App.css";
