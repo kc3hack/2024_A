a82ea209c7846a0559cd3a16d3e61865d4cd734a
JohnDoe-Jack <makaroni.aori0421@gmail.com>
2024-02-24 01:04:41 +0900

    Add import module

--- frontend/pages/Location.tsx
+++ frontend/pages/Location.tsx
@@ -4,6 +4,8 @@ import LocationComponent from "../components/Location";
 import "../styles/Location.css";
 import { store } from "../components/store";
 import { Provider } from "react-redux";
+import React from "react";
+
 const Location = () => {
   return (
     <Provider store={store}>
