b14d8cda6ec80abd7cb2185c61d28934cdc65434
itoxu110 <yitenghuixi@gmail.com>
2024-02-23 20:43:06 +0900

    天気に応じて背景の色が変化する

--- frontend/components/MapBackground.tsx
+++ frontend/components/MapBackground.tsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/MapBackground.css'; // 外部のCSSファイルをインポート
+import React, { useState, useEffect } from "react";
+import "../styles/MapBackground.css"; // 外部のCSSファイルをインポート
 
-const API_KEY = 'api';
+const API_KEY = "api";
 
-const MapBackground = ({latitude, longitude}:{latitude: number; longitude:number;}) => {
+const MapBackground = ({
+  latitude,
+  longitude,
+}: {
+  latitude: number;
+  longitude: number;
+}) => {
   const [weather, setWeather] = useState(null);
   console.log("latitude", latitude, "longitude", longitude);
   useEffect(() => {
@@ -11,23 +17,23 @@ const MapBackground = ({latitude, longitude}:{latitude: number; longitude:number
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
 
       fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           setWeather(data.weather[0].main);
         })
-        .catch(error => {
-          console.error('Error fetching weather data:', error);
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
         });
     }
   }, [latitude, longitude]);
 
-  let weatherClass = 'default';
-  if (weather === 'Clear') {
-    weatherClass = 'clear';
-  } else if (weather === 'Clouds') {
-    weatherClass = 'cloud';
-  } else if (weather === 'Rain') {
-    weatherClass = 'rain';
+  let weatherClass = "default";
+  if (weather === "Clear") {
+    weatherClass = "clear";
+  } else if (weather === "Clouds") {
+    weatherClass = "cloud";
+  } else if (weather === "Rain") {
+    weatherClass = "rain";
   }
 
   return (
