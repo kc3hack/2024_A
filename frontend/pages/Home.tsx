79e25ffa37b3ad32ea18c49cb41a04032944a9c5
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-02-24 09:53:59 +0900

    Home.tsxにMap.cssがインポートされているのを修正

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -2,7 +2,6 @@ import Header from "../components/Header";
 import Map from "../components/Map";
 import Value from "../components/Value";
 import Footer from "../components/Footer";
-import "../styles/Map.css";
 
 const Home = () => {
   return (
