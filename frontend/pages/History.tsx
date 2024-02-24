86b1007e1cf53b4e2f6482602739953a0f029f8b
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-02-24 23:23:03 +0900

    履歴に対応

--- frontend/pages/History.tsx
+++ frontend/pages/History.tsx
@@ -3,18 +3,21 @@ import Footer from "../components/Footer";
 import { getHistory } from "../components/data";
 import { historySort } from "../components/data";
 import { musicData } from "../components/data";
+import { useDispatch } from "react-redux";
+import "../styles/SearchResultPage.css";
 //import { historyAdd } from "../components/data";
+import "../styles/SearchResultPage.css";
 const History = () => {
   historySort();
   const historyID: number[] = getHistory();
+  const dispatch = useDispatch();
   const handleButtonClick = (index: number) => {
-    console.log(`ボタンが押されました。曲名: ${musicData[index].title}`);
-    // ここにボタンが押されたときの処理を追加
+    dispatch({ type: "SET_NEWLY_SELECTED_MUSIC_ID", payload: index }); // 再生中の音楽のIDを設定
+    stop();
   };
-  return (
+/*   return (
     <div>
-      <Header />
-      <h1>ここは履歴のページです</h1>
+      
       <ul id="resultList" className="leftAlign">
         {historyID.length === 0 ? (
           <li className="left-align">履歴がありません</li>
@@ -29,9 +32,39 @@ const History = () => {
           ))
         )}
       </ul>
-      <Footer />
+      
     </div>
   );
+}; */
+return (
+  <div><Header />
+  <div className="app-container">
+    <h1 id="top">履歴</h1>
+    <ul className="result-list">
+      {historyID.length === 0 ? (
+        <li className="result-nothing">履歴がありません</li>
+      ) : (
+        historyID.map((resultIndex) => (
+          <li key={resultIndex} className="result-element border-animation">
+            <span className="music-title">
+              {musicData[resultIndex].title}
+            </span>
+
+            <img
+              src="/play-button.svg"
+              alt=""
+              className="play-music-button"
+              onClick={() => handleButtonClick(resultIndex)}
+            />
+          </li>
+        ))
+      )}
+    </ul>
+    <div className="bg_pattern Diagonal"></div>
+  </div><Footer />
+  </div>
+);
 };
+
 
 export default History;
